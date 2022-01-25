4bed6b961231eb84097e9a8ad57d19a9ef453bc0
vasil <vaskonikolov2003@gmail.com>
2022-01-25 19:11:30 +0200

    starting over utilities

--- screamerProject/src/utilities.h
+++ screamerProject/src/utilities.h
@@ -1,15 +1,13 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
 #include <avr/sleep.h>
 #include <util/delay.h>
 
-extern volatile unsigned int seed, periods_left;
-unsigned int getRandomInteger(unsigned int* seed);
+void setupInterrupt();
 
-void beep();
+void goToSleep();
 #endif
