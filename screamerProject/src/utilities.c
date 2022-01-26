49fe59a51c88c57223325ec53d0807e63ae21340
vasil <vaskonikolov2003@gmail.com>
2022-01-26 17:24:52 +0200

    removed useless includes

--- screamerProject/src/utilities.c
+++ screamerProject/src/utilities.c
@@ -1,32 +1,6 @@
-#include <avr/io.h>
-#include <avr/interrupt.h>
-#include <avr/wdt.h>
-#include <avr/sleep.h>
-#include <util/delay.h>
-
 #include "utilities.h"
 #include "projectConstants.h"
 
-<<<<<<< HEAD
-volatile unsigned int seed, periods_left;
-
-unsigned int getRandomInteger(unsigned int* seed){
-    unsigned int a = 1687;
-    unsigned int m = 257;
-    *seed = (a * (*seed)) % m;
-    return (unsigned int)(*seed);
-}
-
-void beep(){
-    // short n_beeps = getRandomInteger(&seed) % 5 + 3;
-    short n_beeps = 5;
-    for(short i = 0; i < n_beeps; i++){
-        PORTB |= (1 << BEEP_PIN_1) | (1 << BEEP_PIN_2);
-        _delay_ms(BEEP_TIME_ON);
-        PORTB &= ~((1 << BEEP_PIN_1) | (1 << BEEP_PIN_2));
-        _delay_ms(BEEP_TIME_OFF);
-    }    
-=======
 void setupInterrupt(){
     sei();
     GIMSK |= (1 << PCIE);
@@ -69,5 +43,4 @@ void beep(){
 
 ISR(PCINT0_vect){
 
->>>>>>> startingOver
 }
